c6de2ec1e4e422c1fd9d710b856de6e9dfe85b1d
Bilal Waseem Ahmed <bilalwaseem.thala@gmail.com>
2025-04-21 00:12:26 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+// next.config.js
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-};
-
-export default nextConfig;
+  webpack(config) {
+    return config;
+  },
+  // Tell Next.js to look in the `src` folder for pages
+  srcDir: 'src',
+}
