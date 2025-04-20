740e1140b40e6e0343b60ee7fa5ca46d8d13b151
Bilal Waseem Ahmed <bilalwaseem.thala@gmail.com>
2025-04-21 00:02:11 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Add the `src` directory support
+  srcDir: 'src', // Tell Next.js to look in the `src` folder for pages
 };
 
 export default nextConfig;
